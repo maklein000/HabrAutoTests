ad96c731115ab847a740dca25d7672904bbf1a58
mormont <mak.lein000@gmail.com>
2022-07-23 22:16:18 +0300

    Finish autotest about search articles

--- first_try.py
+++ first_try.py
@@ -5,21 +5,21 @@ from selenium.webdriver.common.by import By
 
 driver = webdriver.WebDriver(executable_path="chromedriver")
 
-driver.get("https://habr.com")
+driver.get("https://habr.com/ru/all/")
 
 
-time.sleep(1)
+time.sleep(3)
 
-search_button_locator = By.CSS_SELECTOR, '[data-test-id="search-button"]'
+search_button_locator = By.CLASS_NAME, 'tm-header-user-menu__search'
 
 search_button = driver.find_element(*search_button_locator)
 search_button.click()
-time.sleep(1)
+time.sleep(3)
 
 # вбить текст
 search_input_locator = By.CLASS_NAME, 'tm-input-text-decorated__input'
 search_input = driver.find_element(*search_input_locator)
-text_to_search = 'Selenium'
+text_to_search = 'Жопа'
 search_input.send_keys(text_to_search)
 
 # нажать на иконку
@@ -27,13 +27,16 @@ search_icon_locator = By.CSS_SELECTOR, 'span.tm-search__icon'
 search_icon = driver.find_element(*search_icon_locator)
 search_icon.click()
 
-time.sleep(20)
+time.sleep(3)
+
 # посчитать количество записей(20)
+article_locator = By.TAG_NAME, 'article'
+articles = driver.find_elements(*article_locator)
+print(f'Number of articles is {len(articles)}')
 
 # посчитать количество страниц(50)
-
-
-
-
-
-
+last_page_locator = By.XPATH, '(//*[@class="tm-pagination__page"])[last()]'
+last_page_number = driver.find_element(*last_page_locator)
+element_text = last_page_number.text
+print (f'Number of pages is {element_text}')
+driver.quit()
