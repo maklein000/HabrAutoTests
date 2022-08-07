fbe50635da46cdc602f75c6c5c83ee2b0582c245
maklein000 <mak.lein000@gmail.com>
2022-08-07 17:25:16 +0300

    Minor changes in README.md

--- first_try.py
+++ first_try.py
@@ -1,25 +1,24 @@
-from selenium.webdriver.chrome import webdriver
 import time
 
+from selenium.webdriver.chrome import webdriver
 from selenium.webdriver.common.by import By
 
 driver = webdriver.WebDriver(executable_path="chromedriver")
 
 driver.get("https://habr.com/ru/all/")
 
-
-time.sleep(3)
+time.sleep(1)
 
 search_button_locator = By.CLASS_NAME, 'tm-header-user-menu__search'
 
 search_button = driver.find_element(*search_button_locator)
 search_button.click()
-time.sleep(3)
+time.sleep(1)
 
 # вбить текст
 search_input_locator = By.CLASS_NAME, 'tm-input-text-decorated__input'
 search_input = driver.find_element(*search_input_locator)
-text_to_search = 'Жопа'
+text_to_search = 'NFT'
 search_input.send_keys(text_to_search)
 
 # нажать на иконку
@@ -27,7 +26,7 @@ search_icon_locator = By.CSS_SELECTOR, 'span.tm-search__icon'
 search_icon = driver.find_element(*search_icon_locator)
 search_icon.click()
 
-time.sleep(3)
+time.sleep(1)
 
 # посчитать количество записей(20)
 article_locator = By.TAG_NAME, 'article'
@@ -38,5 +37,6 @@ print(f'Number of articles is {len(articles)}')
 last_page_locator = By.XPATH, '(//*[@class="tm-pagination__page"])[last()]'
 last_page_number = driver.find_element(*last_page_locator)
 element_text = last_page_number.text
-print (f'Number of pages is {element_text}')
+print(f'Number of pages is {element_text}')
+
 driver.quit()
