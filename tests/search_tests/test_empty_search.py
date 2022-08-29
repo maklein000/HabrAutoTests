c00523b4da033e22d93e45b05491ed46d0bdc235
mormont <mak.lein000@gmail.com>
2022-08-29 15:32:13 +0300

    Move page opening to local contest

--- tests/search_tests/test_empty_search.py
+++ tests/search_tests/test_empty_search.py
@@ -1,23 +1,9 @@
-from page_objects.page import *
-
-
-def test_empty_search(driver):
-    page = MainPage(driver)
-    page.open()
-
-    page = page.click_search()
-
+def test_empty_search(page):
     page.search('kkgkgk')
 
     assert page.count_articles_number() == 0
 
 
-def test_empty_search_text(driver):
-    page = MainPage(driver)
-    page.open()
-
-    page = page.click_search()
-
+def test_empty_search_text(page):
     page.search('kkgkgk')
-
     assert len(page.get_empty_page_text()) > 0
--- tests/search_tests/test_pagination.py
+++ tests/search_tests/test_pagination.py
@@ -1,17 +1,8 @@
-from page_objects.page import *
-
-
-def test_pagination_to_last_page(driver):
-    page = MainPage(driver)
-    page.open()
-
-    page = page.click_search()
-
+def test_pagination_to_last_page(page):
     page.search('NFT')
 
     page.go_to_last_page()
 
     expected = 'https://habr.com/ru/search/page19/?q=NFT&target_type=posts&order=relevance'
     actual = page.current_url
-
     assert actual == expected
