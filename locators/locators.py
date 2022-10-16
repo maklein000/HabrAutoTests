0432b1eccea24fc5c6ebe3268784f1ec9adb1373
mormont <mak.lein000@gmail.com>
2022-10-16 17:39:39 +0300

    add tests for feedback, rename some files

--- locators/locators.py
+++ locators/locators.py
@@ -13,3 +13,11 @@ services_dropdown_element = By.CLASS_NAME, 'tm-our-projects__item'
 search_icon_locator = By.CSS_SELECTOR, 'span.tm-search__icon'
 search_input_locator = By.CLASS_NAME, 'tm-input-text-decorated__input'
 empty_res_locator = By.XPATH, '//*[@data-test-id="empty-placeholder-text"]'
+
+# Feedback Page
+submit_button = By.XPATH, '//*[contains(@class, "tm-form__submit")]'
+email_error = By.XPATH, '//*[contains(@class, "base-input__error_email")]'
+message_error = By.XPATH, '//*[contains(@class, "base-input__error_message")]'
+agreement_error = By.XPATH, '//*[contains(@class, "base-checkbox__error_personalagreement")]'
+subject = By.NAME, 'themeId'
+subject_option = By.CSS_SELECTOR, 'select[name="themeId"] option'
--- page_objects/feedback_page.py
+++ page_objects/feedback_page.py
@@ -1,5 +1,35 @@
+from locators.locators import submit_button, email_error, message_error, agreement_error, subject_option, subject
 from page_objects.base_page import HabrBase
+from selenium.webdriver.support.ui import Select
 
 
 class FeedbackPage(HabrBase):
-    pass
+    url = 'https://habr.com/ru/feedback/'
+
+    @property
+    def submit_button(self):
+        return self.webdriver.find_element(*submit_button)
+
+    @property
+    def email_error_message(self):
+        return self.webdriver.find_element(*email_error)
+
+    @property
+    def message_error_message(self):
+        return self.webdriver.find_element(*message_error)
+
+    @property
+    def personal_agreement_error_message(self):
+        return self.webdriver.find_element(*agreement_error)
+
+    @property
+    def subject(self):
+        return Select(self.webdriver.find_element(*subject))
+
+    def count_subject(self):
+        elements = self.subject.options
+
+        return len(elements)
+
+    def change_subject(self, index=1):
+        self.subject.select_by_index(index)
